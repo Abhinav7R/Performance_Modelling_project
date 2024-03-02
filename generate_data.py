5064e6f1bbcc0a35e67e9a0be6a5d132adefff77
Abhinav Raundhal <abhinavraundhal@Abhinavs-MacBook-Air.local>
2024-03-02 06:44:45 +0530

    final ig

--- generate_data.py
+++ generate_data.py
@@ -17,12 +17,19 @@ def generate_exponential_xi(n, lmbda):
     x = [-np.log(1 - y[i]) / lmbda for i in range(len(y))]
     return x
 
+def generate_exp(n, lmbda):
+    return np.random.exponential(lmbda, n)
+
 def generate_si_sigma_xi(n):
-    n_sorted = sorted(n)
+    n_sorted = n
     si_sigma_xi = []
     for i in range(len(n_sorted)):
         si_sigma_xi.append(sum(n_sorted[:i+1]))
     return si_sigma_xi
+
+def generate_si_cumulative(n):
+    cumulative_service_time = np.cumsum(n)
+    return cumulative_service_time
 
 def generate_sizes(si_sigma_xi, mu):
     sizes = generate_exponential_xi(len(si_sigma_xi), mu)
@@ -40,9 +47,18 @@ def assign_priority(jobsArray, number_of_queues, average_service_time):
         job.priority = priority
     return jobsArray
 
+def assign_priority_changed(jobsArray, number_of_queues, average_service_time):
+    bin_size = 1 / number_of_queues
+    for job in jobsArray:
+        priority = 1 - np.exp(-lmbda * job.service_time)
+        priority = int(priority / bin_size)
+        job.priority = priority
+    return jobsArray
 
+# xi = generate_exp(n, lmbda)
 xi = generate_exponential_xi(n, lmbda)
 si_sigma_xi = generate_si_sigma_xi(xi)
+# si_sigma_xi = generate_si_cumulative(xi)
 jobsArray = generate_sizes(si_sigma_xi, mu)
 jobsArray = assign_priority(jobsArray, number_of_queues, 1/mu)    
 
@@ -56,3 +72,5 @@ with open('jobs.csv', mode='w') as jobs_file:
     jobs_writer.writerow(['arrival_time', 'service_time', 'priority'])
     for job in jobsArray:
         jobs_writer.writerow([job.arrival_time, job.service_time, job.priority])
+
+print("Data generated and written to jobs.csv")
